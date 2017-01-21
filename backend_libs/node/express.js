77007eee7dccbb642c42a275107707b2f3ae4343
Ryan Jadidi <ryan.jadidi@sap.com>
2017-01-21 13:47:45 +1000

    flow and eslint play nicley now

--- backend_libs/node/express.js
+++ backend_libs/node/express.js
@@ -1,13 +1,14 @@
 // @flow
 const {nodeEnv} = require('./util');
+/*eslint-disable */
 console.log(`Running in ${nodeEnv} mode!`);
-
+/*eslint-enable */
 const app = require("express")();
-const schema = require("../schema");
+const schema = require("../schema/model");
 const graphqlHTTP = require("express-graphql");
 const {MongoClient} = require("mongodb");
 const assert = require("assert");
-const mongoConfig = require("../config/mongo")[nodeEnv];
+const mongoConfig = require("../config/main_config")[nodeEnv];
 
 MongoClient.connect(mongoConfig.url, (err, mPool) => {
     assert.equal(err, null);
@@ -18,6 +19,8 @@ MongoClient.connect(mongoConfig.url, (err, mPool) => {
     }));
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
+        /*eslint-disable */
         console.log(`server is listening on port ${PORT}`);
+        /*eslint-enable */
     });
 });
