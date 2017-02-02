74070157bb2fe4ea1395f7312bc16f4239097267
Ryan Jadidi <ryan.jadidi@sap.com>
2017-02-02 20:46:59 +1000

    progressing with registration page look.

--- react_native/Registration.js
+++ react_native/Registration.js
@@ -1,37 +1,58 @@
 // @flow
 import React, {Component} from 'react';
 import {
-  StyleSheet,
-  TextInput,
-  View
+  StyleSheet, 
+  TextInput, TouchableHighlight,
+  Text, Button, View
 } from 'react-native';
 
 export default class Registration extends Component {
   render() {
     return (
       <View style={styles.container}>
+        <Text style={styles.text}>
+          Welcome to ping4game!
+        </Text>
         <TextInput style={styles.textInput} placeholder="choose an id"/>
         <TextInput style={styles.textInput} placeholder="add your email"/>
         <TextInput style={styles.textInput} placeholder="current location"/>
+        <TouchableHighlight onPress={this.onPressButton}>
+          <View>
+          <Button style={styles.submitButton} title="Submit"></Button>
+          </View>
+        </TouchableHighlight>
       </View>
     );
   }
+  onButtonPress(event) {}
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#5CD6F4'
+    justifyContent: 'flex-start',
+    backgroundColor: '#0398bc'
   },
   textInput: {
     alignSelf: 'center',
     textAlign: 'center',
     width: 300,
     height: 70,
-    marginTop: 50,
+    marginTop: 40,
     borderRadius: 20,
     overflow: 'hidden',
     backgroundColor: 'white'
+  },
+  text: {
+    textAlign: 'center',
+    marginTop: 80,
+    marginBottom: 50,
+    fontWeight: 'bold',
+    fontSize: 16
+  },
+  submitButton: {
+    textAlign: 'center',
+    color: '#008080',
+    marginTop: 40
   }
 });
