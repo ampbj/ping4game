321ba2ea6ed17f36d63148df735cce60f52243fc
Ryan Jadidi <ryan.jadidi@sap.com>
2017-02-14 08:39:37 +1000

    getting realy mutation ready

--- relay/Ping4GameRoute.js
+++ relay/Ping4GameRoute.js
@@ -4,8 +4,8 @@ export default class Ping4GameRoute extends Relay.Route {
   static routeName = 'Ping4GameRoute';
   static queries = {
     registration: (Component) => Relay.QL`
-      query RegistrationQuery {
-          registration { ${Component.getFragment('user')} },
+      query RootQuery {
+          userType { ${Component.getFragment('userType')} },
       }
     `
   };
